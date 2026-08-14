6e90d1ed88f770845fd52108a7727b54ca1780e0
Oscar <off.jc27@gmail.com>
2026-08-14 14:08:35 -0500

    primera subida

---
+++ backend/src/config/env.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+const requiredEnvVars = ['DB_HOST','DB_USER','DB_NAME','JWT_SECRET'];
+
+for (const name of required){
+    if (!process.env[name]) {
+        throw new Error(`Falta la variable de entorno: ${name}`);
+    }
+}
+
+module.exports = {
+    port: Number(process.env.PORT || 3000),
+    db: {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT || 3306),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD || '',
+        database: process.env.DB_NAME
+    },
+    jwt: {
+        secret: process.env.JWT_SECRET,
+        expiresIn: process.env.JWT_EXPIRES_IN || '2h',
+    }
+};
